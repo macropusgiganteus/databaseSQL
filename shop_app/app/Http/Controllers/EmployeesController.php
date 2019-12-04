4bc0385c2f30a30a4c8a00533e4f735004df6725
macropusgiganteus <35326900+macropusgiganteus@users.noreply.github.com>
2019-12-04 13:42:58 +0700

    update employee

--- shop_app/app/Http/Controllers/EmployeesController.php
+++ shop_app/app/Http/Controllers/EmployeesController.php
@@ -7,33 +7,18 @@ use Illuminate\Http\Request;
 
 class EmployeesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $employees = Employees::all()->toArray();
         return view('employee.employees', compact('employees'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('employee.addEmployees');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, ['employeeNumber' => 'required',
@@ -60,36 +45,12 @@ class EmployeesController extends Controller
         return redirect('/employees');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($employeeNumber)
     {
         $employees = Employees::where('employeeNumber', $employeeNumber)->first();
         return view('employee.editEmployee', compact('employees', 'employeeNumber'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $employeeNumber)
     {
         $this->validate($request,
@@ -102,26 +63,20 @@ class EmployeesController extends Controller
                 'jobTitle' => 'required',
                 'extension' => 'required',
             ]);
-        $employees = Employees::where('employeeNumber', $employeeNumber)->first();
-        $employees->employeeNumber = $request->get('employeeNumber');
-        $employees->firstName = $request->get('firstName');
-        $employees->lastName = $request->get('lastName');
-        $employees->email = $request->get('email');
-        $employees->officeCode = $request->get('officeCode');
-        $employees->reportsTo = $request->get('reportsTo');
-        $employees->jobTitle = $request->get('jobTitle');
-        $employees->extension = $request->get('extension');
+        $employees = Employees::where('employeeNumber', $request->get('employeeNumber'))->first();
+        $employees->employeeNumber = $request->input('employeeNumber');
+        $employees->firstName = $request->input('firstName');
+        $employees->lastName = $request->input('lastName');
+        $employees->email = $request->input('email');
+        $employees->officeCode = $request->input('officeCode');
+        $employees->reportsTo = $request->input('reportsTo');
+        $employees->jobTitle = $request->input('jobTitle');
+        $employees->extension = $request->input('extension');
         $employees->timestamps = false;
         $employees->save();
         return redirect('/employees');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($employeeNumber)
     {
         $employees = Employees::where('employeeNumber', $employeeNumber);
--- shop_app/resources/views/employee/editEmployee.blade.php
+++ shop_app/resources/views/employee/editEmployee.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.AllLayout')
 @section('title','SHOP')
 @section('content')
-<section id="add" class="container">
+<section class="container">
     <div class="form-wrap">
       <h1>Edit Employee informations</h1>
       
       <form action="{{action('EmployeesController@update', $employeeNumber)}} method="POST">
+          @csrf
+          @method('PUT')
         {{csrf_field()}}
         <div class="input-group">
           <label for="title">Job title</label>
@@ -49,17 +51,5 @@
     </div>
   </section>
 
-  <section class="container">
-    @if(count($errors)>0)
-    <div class="w3-panel w3-red"align="center">
-      <ul>
-        @foreach ($errors->all() as $error)
-      <li>{{$error}}</li>   
-        @endforeach
-      </ul>
-    </div>
-    @endif
-
-</section>
 
 @endsection
